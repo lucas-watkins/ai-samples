38b81288f5f72d6ec1a6a7f2ce0b9584cccea799
lucas-watkins <lucas-watkins@outlook.com>
2024-01-13 11:22:23 -0500

    working code in theory

--- ai_sample.py
+++ ai_sample.py
@@ -32,39 +32,43 @@ class Streamer:
         self.print_len = 0
         self.next_tokens_are_prompt = True
 
+        #end of text variable
+        self.end_of_total_text = False
+
     def put(self, value):
-        """
-        Receives tokens, decodes them, and prints them to stdout as soon as they form entire words.
-        """
-        if len(value.shape) > 1 and value.shape[0] > 1:
-            raise ValueError("TextStreamer only supports batch size 1")
-        elif len(value.shape) > 1:
-            value = value[0]
+        if not self.end_of_total_text:
+            """
+            Receives tokens, decodes them, and prints them to stdout as soon as they form entire words.
+            """
+            if len(value.shape) > 1 and value.shape[0] > 1:
+                raise ValueError("TextStreamer only supports batch size 1")
+            elif len(value.shape) > 1:
+                value = value[0]
 
-        if self.skip_prompt and self.next_tokens_are_prompt:
-            self.next_tokens_are_prompt = False
-            return
+            if self.skip_prompt and self.next_tokens_are_prompt:
+                self.next_tokens_are_prompt = False
+                return
 
-        # Add the new token to the cache and decodes the entire thing.
-        self.token_cache.extend(value.tolist())
-        text = self.tokenizer.decode(self.token_cache, **self.decode_kwargs)
+            # Add the new token to the cache and decodes the entire thing.
+            self.token_cache.extend(value.tolist())
+            text = self.tokenizer.decode(self.token_cache, **self.decode_kwargs)
 
-        # After the symbol for a new line, we flush the cache.
-        if text.endswith("\n"):
-            printable_text = text[self.print_len :]
-            self.token_cache = []
-            self.print_len = 0
-        # If the last token is a CJK character, we print the characters.
-        elif len(text) > 0 and self._is_chinese_char(ord(text[-1])):
-            printable_text = text[self.print_len :]
-            self.print_len += len(printable_text)
-        # Otherwise, prints until the last space char (simple heuristic to avoid printing incomplete words,
-        # which may change with the subsequent token -- there are probably smarter ways to do this!)
-        else:
-            printable_text = text[self.print_len : text.rfind(" ") + 1]
-            self.print_len += len(printable_text)
+            # After the symbol for a new line, we flush the cache.
+            if text.endswith("\n"):
+                printable_text = text[self.print_len :]
+                self.token_cache = []
+                self.print_len = 0
+            # If the last token is a CJK character, we print the characters.
+            elif len(text) > 0 and self._is_chinese_char(ord(text[-1])):
+                printable_text = text[self.print_len :]
+                self.print_len += len(printable_text)
+            # Otherwise, prints until the last space char (simple heuristic to avoid printing incomplete words,
+            # which may change with the subsequent token -- there are probably smarter ways to do this!)
+            else:
+                printable_text = text[self.print_len : text.rfind(" ") + 1]
+                self.print_len += len(printable_text)
 
-        self.on_finalized_text(printable_text)
+            self.on_finalized_text(printable_text)
 
     def end(self):
         """Flushes any remaining cache and prints a newline to stdout."""
@@ -82,11 +86,11 @@ class Streamer:
 
     def on_finalized_text(self, text: str, stream_end: bool = False):
         """Prints the new text to stdout. If the stream is ending, also prints a newline."""
-        if not '<|endoftext|>' in text:
+        if not '<|endoftext|>' in text and not self.end_of_total_text:
             print(text, flush=True, end="" if not stream_end else '\n')
         else:
-            #flush cache
-            self.end()
+            if not self.end_of_total_text:
+                self.end_of_total_text = True
 
 
     def _is_chinese_char(self, cp):
